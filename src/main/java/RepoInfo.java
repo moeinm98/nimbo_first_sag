cacf26f0bec2892eccba8e1a785e4ea06cb5b310
Ali Ehteshami <ali.ehteshami7@gmail.com>
2017-07-31 18:28:38 +0430

    changed name formats to static and uppercase

--- src/main/java/RepoInfo.java
+++ src/main/java/RepoInfo.java
@@ -1,11 +1,29 @@
-public class RepoInfo extends Info{
+public class RepoInfo extends Info {
+
+    final static private int COMMIT_COMMENT_PRIORITY = 1;
+    final static private int CREATION_PRIORITY = 2;
+    final static private int FORK_PRIORITY = 5;
+    final static private int ISSUE_COMMENT_PRIORITY = 2;
+    final static private int ISSUE_PRIORITY = 3;
+    final static private int LABEL_PRIORITY = 2;
+    final static private int MILESTONE_PRIORITY = 2;
+    final static private int PAGE_BUILD_PRIORITY = 2;
+    final static private int PULL_REQUEST_PRIORITY = 3;
+    final static private int COMMIT_PRIORITY = 3;
+    final static private int RELEASE_PRIORITY = 5;
+    final static private int GOLLUM_PRIORITY = 3;
+    final static private int WATCH_PRIORITY = 5;
 
     public RepoInfo(String repoName) {
         super(repoName);
     }
 
     @Override
-    public void computeValue() {
-
+    public int computeValue() {
+        int score = COMMIT_COMMENT_PRIORITY * getCommitCommentNum() + CREATION_PRIORITY * getCreationNum() + FORK_PRIORITY * getForkNum() + ISSUE_COMMENT_PRIORITY * getIssueCommentNum();
+        score += ISSUE_PRIORITY * getIssueNum() + LABEL_PRIORITY * getLabelNum() + MILESTONE_PRIORITY * getMilestoneNum() + PAGE_BUILD_PRIORITY * getPageBuildNum();
+        score += PULL_REQUEST_PRIORITY * getPullRequestNum() + COMMIT_PRIORITY * getCommitNum() + RELEASE_PRIORITY * getReleaseNum() + GOLLUM_PRIORITY * getGollumNum();
+        score += WATCH_PRIORITY * getWatchNum();
+        return score;
     }
 }
