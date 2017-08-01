7ae39c07c997daced71e5ffebfbaf021afe250d1
Ali Ehteshami <ali.ehteshami7@gmail.com>
2017-08-01 14:58:48 +0430

    make code cleaner by making method updateOutputFile

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -13,6 +13,10 @@ public class Engine {
     private Semaphore oneHourTimerSemaphore = new Semaphore(0);
     private Semaphore oneDayTimerSemaphore = new Semaphore(0);
     private File backupFile;
+    private File tenMinResults = new File("tenMinTrends.txt");
+    private File oneHourResults = new File("oneHourTrends.txt");
+    private File oneDayResults = new File("oneDayTrends.txt");
+
     private int time; //number of 10 mins passed
 
     public void start() {
@@ -22,12 +26,13 @@ public class Engine {
         FileWriter fileWriterBackup;
         BufferedWriter bufferedWriterBackup = null;
 
-        if (!backupFile.exists()) {
-            try {
-                backupFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try {
+            backupFile.createNewFile();
+            tenMinResults.createNewFile();
+            tenMinResults.createNewFile();
+            tenMinResults.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         try {
@@ -51,48 +56,18 @@ public class Engine {
             @Override
             public void run() {
                 time++;
-                File tenMinResults = new File("tenMinTrends.txt");
-                FileWriter fileWriterTenMin;
-                BufferedWriter bufferedWriterTenMin = null;
-                if (!tenMinResults.exists()) {
-                    try {
-                        tenMinResults.createNewFile();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                try {
-                    fileWriterTenMin = new FileWriter(tenMinResults, true);
-                    bufferedWriterTenMin = new BufferedWriter(fileWriterTenMin);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
                 String[] trends = tenMinStatistics.findAndGetTrends();
                 oneHourStatistics.mergeStatistics(tenMinStatistics);
                 tenMinStatistics.clearStatistics();
-                //<<<<<<<<<<<
-                FileWriter fileWriter;
-
-                try { //todo ehtemalan kar nakhahad kard
-                    fileWriter = new FileWriter(backupFile);
+                try {
+                    FileWriter fileWriter = new FileWriter(backupFile);
                     fileWriter.write("");
                     fileWriter.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                //>>>>>>>>>>>
-
-                try {
-                    bufferedWriterTenMin.write("\n\n" + time * 10 + " minutes\nuserName : " + trends[0] + "\nrepoName : " + trends[1] + "\nlanguage : " + trends[2]);
-                    bufferedWriterTenMin.close();
-                    System.out.println("tenMinTrends.txt updated!");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+                updateOutputFile(tenMinResults, trends, time * 10, "Minutes");
                 oneHourTimerSemaphore.release();
-
             }
         }, 1000 * 60 * 10, 1000 * 60 * 10);
 
@@ -106,36 +81,10 @@ public class Engine {
                         e.printStackTrace();
                     }
                 }
-
-                File oneHourResults = new File("oneHourTrends.txt");
-                FileWriter fileWriterOneHour;
-                BufferedWriter bufferedWriterOneHour = null;
-                if (!oneHourResults.exists()) {
-                    try {
-                        oneHourResults.createNewFile();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                try {
-                    fileWriterOneHour = new FileWriter(oneHourResults, true);
-                    bufferedWriterOneHour = new BufferedWriter(fileWriterOneHour);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
                 String[] trends = oneHourStatistics.findAndGetTrends();
                 oneDayStatistics.mergeStatistics(oneHourStatistics);
                 oneHourStatistics.clearStatistics();
-
-                try {
-                    bufferedWriterOneHour.write("\n\n" + time / 6 + " hours\nuserName : " + trends[0] + "\nrepoName : " + trends[1] + "\nlanguage : " + trends[2]);
-                    bufferedWriterOneHour.close();
-                    System.out.println("oneHourTrends.txt updated!");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+                updateOutputFile(oneHourResults, trends, time / 6, "Hour");
                 oneDayTimerSemaphore.release();
             }
         }, 1000 * 60 * 60, 1000 * 60 * 60);
@@ -150,36 +99,22 @@ public class Engine {
                         e.printStackTrace();
                     }
                 }
-                File oneDayResults = new File("oneDayTrends.txt");
-                FileWriter fileWriterOneDay;
-                BufferedWriter bufferedWriterOneDay = null;
-                if (!oneDayResults.exists()) {
-                    try {
-                        oneDayResults.createNewFile();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                try {
-                    fileWriterOneDay = new FileWriter(oneDayResults, true);
-                    bufferedWriterOneDay = new BufferedWriter(fileWriterOneDay);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
                 String[] trends = oneDayStatistics.findAndGetTrends();
                 oneDayStatistics.clearStatistics();
-                try {
-                    bufferedWriterOneDay.write("\n\n" + time / (6 * 24) + " days\nuserName : " + trends[0] + "\nrepoName : " + trends[1] + "\nlanguage : " + trends[2]);
-                    bufferedWriterOneDay.close();
-                    System.out.println("oneHourTrends.txt updated!");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+                updateOutputFile(oneDayResults, trends, time / (6 * 24), "Day");
             }
         }, 1000 * 60 * 60 * 24, 1000 * 60 * 60 * 24);
+    }
 
-
+    private void updateOutputFile(File file, String[] trends, int time, String timeUnit) {
+        try {
+            FileWriter fileWriter = new FileWriter(oneDayResults, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("\n\n" + time + " " + timeUnit + "\nuserName : " + trends[0] + "\nrepoName : " + trends[1] + "\nlanguage : " + trends[2]);
+            bufferedWriter.close();
+            System.out.println(file.getName() + " UPDATED!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
