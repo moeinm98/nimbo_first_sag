f77fc3a9e8e23ea0874a35111cbfe28942b7290e
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2017-08-02 15:00:29 +0430

    Create BackupHandler class.

---
+++ src/main/java/BackupHandler.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class BackupHandler
+{
+    public static void updateBackupFile(String backupFileName, Statistics statistics) throws IOException
+    {
+        FileOutputStream backupFileOutputStream = new FileOutputStream(backupFileName);
+        ObjectOutputStream backupObjectOutputStream = new ObjectOutputStream(backupFileOutputStream);
+        backupObjectOutputStream.writeObject(statistics);
+    }
+
+    public static Statistics getBackupIfExists(File backupFile) throws IOException, ClassNotFoundException
+    {
+        if (backupFile.exists()) {
+            FileInputStream receivingFileInputStream = new FileInputStream(backupFile);
+            ObjectInputStream receivingObjectInputStream = new ObjectInputStream(receivingFileInputStream);
+            return (Statistics) receivingObjectInputStream.readObject();
+        }
+
+        return new Statistics();
+    }
+}
